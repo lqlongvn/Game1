d8004b3da8fcfae9c5f5ede4cb7b0bc8c780ae93
Long <lqlongvn@gmail.com>
2018-05-28 21:24:08 +0700

    Game Rocket. Time: 28/5/18; 21h30; Version 2.2; Kết quả cuối cùng của Buổi 5 (còn nhiều lỗi);

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,6 @@
 import java.awt.*;
+import java.util.Arrays;
+import java.util.List;
 import java.awt.image.BufferedImage;
 
 public class Player {
@@ -7,6 +9,8 @@ public class Player {
     public int y;
     public int width;
     public int height;
+    private List<Vector2D> verties;
+    private Polygon polygon;
 
     public Player(BufferedImage image, int x, int y, int width, int height) {
         this.image = image;
@@ -14,11 +18,22 @@ public class Player {
         this.y = y;
         this.width = width;
         this.height = height;
+        this.verties = Arrays.asList (
+                new Vector2D(),
+                new Vector2D(0,16),
+                new Vector2D(20,8)
+        );
+        this.polygon = new Polygon();
+
 
     }
 
     public void render(Graphics graphics) {
-        graphics.drawImage(this.image, this.x, this.y, this.width, this.height, null);
+        //graphics.drawImage(this.image, this.x, this.y, this.width, this.height, null);
+        graphics.setColor(Color.RED);
+        this.polygon.reset();
+        this.verties.forEach(vertex->polygon.addPoint((int) vertex.x,(int) vertex.y));
+        graphics.drawPolygon(this.polygon);
     }
 
 
